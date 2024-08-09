e147ccdaf780a3bd4c5f88010fa53b7d30d2c1c7
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-08-09 11:23:41 +0500

    OpenZeppelin and account-abstraction contract added

---
+++ src/Ethereum/MinimalAccount.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
