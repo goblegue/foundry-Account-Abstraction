bff3e943c66eca8cd51283034e53aaec83c80cb0
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-08-10 17:54:00 +0500

    HelperConfig add

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        address entryPoint;
+        address account;
+    }
+
+    uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
+    uint256 constant LOCAL_CHAIN_ID = 31337;
+
+    address constant BURNER_WALLET = 0x6D966303C161995E42e695a4E8336aCF4CA77ECb;
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+    NetworkConfig public localNetworkConfig;
+
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
+        networkConfigs[ZKSYNC_SEPOLIA_CHAIN_ID] = getZKsyncSepoliaConfig();
+    }
+
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+        if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilConfig();
+        }
+
+        else if (networkConfigs[chainId].account != address(0)) {
+            return networkConfigs[chainId];
+        }
+        else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function getEthSepoliaConfig() public pure returns (NetworkConfig memory ethSepoliaNetworkConfig) {
+        ethSepoliaNetworkConfig =
+            NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, account: BURNER_WALLET});
+    }
+
+    function getZKsyncSepoliaConfig() public pure returns (NetworkConfig memory ethSepoliaNetworkConfig) {
+        ethSepoliaNetworkConfig = NetworkConfig({entryPoint: address(0), account: BURNER_WALLET});
+    }
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
+        if (localNetworkConfig.account != address(0)) {
+            return localNetworkConfig;
+        }
+    }
+}
