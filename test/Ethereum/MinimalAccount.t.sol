98b97d1667e1030e1eba6216c1381ea0043a9aff
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-08-11 17:43:33 +0500

    more test added

--- test/Ethereum/MinimalAccount.t.sol
+++ test/Ethereum/MinimalAccount.t.sol
@@ -7,8 +7,16 @@ import {MinimalAccount} from "../../src/Ethereum/MinimalAccount.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DeployMinimalAccount} from "../../script/DeployMinimalAccount.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp} from "../../script/SendPackedUserOp.s.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract MinimalAccountTest is Test {
+    using MessageHashUtils for bytes32;
+
+    SendPackedUserOp sendPackedUserOp;
     HelperConfig helperConfig;
     MinimalAccount minimalAccount;
 
@@ -16,11 +24,14 @@ contract MinimalAccountTest is Test {
 
     address randomUser = makeAddr("randomUser");
     uint256 constant AMOUNT = 1e18;
+
     function setUp() public {
         DeployMinimalAccount deployMinimalAccount = new DeployMinimalAccount();
         (helperConfig, minimalAccount) = deployMinimalAccount.deployMinimalAccount();
         usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
     }
+
     function testOwnerCanEXecute() public {
         //Arrange
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
@@ -32,7 +43,6 @@ contract MinimalAccountTest is Test {
         minimalAccount.execute(dest, value, functionData);
         //Assert
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
-
     }
 
     function testNonOwnerCannotExecute() public {
@@ -45,11 +55,46 @@ contract MinimalAccountTest is Test {
         vm.prank(randomUser);
         vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
         minimalAccount.execute(dest, value, functionData);
+    }
 
+    function testRecoverSignedUserOp() public {
+        //Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        bytes memory executeCallData =
+            abi.encodeWithSelector(MinimalAccount.execute.selector, dest, value, functionData);
+
+        PackedUserOperation memory userOp = sendPackedUserOp.getSignedUserOp(executeCallData, helperConfig.getConfig());
+
+        bytes32 userOpHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(userOp);
+
+        //Act
+        address actualSigner = ECDSA.recover(userOpHash.toEthSignedMessageHash(), userOp.signature);
+
+        //Assert
+        assertEq(actualSigner, minimalAccount.owner());
+    }
+
+    function testValidationOfUserOp() public {
+        //Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        bytes memory executeCallData =
+            abi.encodeWithSelector(MinimalAccount.execute.selector, dest, value, functionData);
+
+        PackedUserOperation memory userOp = sendPackedUserOp.getSignedUserOp(executeCallData, helperConfig.getConfig());
+
+        bytes32 userOpHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(userOp);
+        uint256 missingAccountFunds = 1e18;
+
+        //Act
+        vm.prank(helperConfig.getConfig().entryPoint);
+        uint256 validationData = minimalAccount.validateUserOp(userOp,userOpHash,missingAccountFunds);
+        //Assert
+        assertEq(validationData,0);
     }
 }
-        
-
-
-
-
